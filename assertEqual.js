36ee601dfa9ac4cc83914f287cbf7498ddc954df
Mariya <va.mariya@yahoo.com>
2021-10-28 00:23:41 +0000

    assertEquals

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  let a = actual;
+  let b = expected;
+  if (a === b) {
+    console.log('Assertion Passed: '  + actual + ' === ' + expected);
+  } else {
+    console.log('Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
