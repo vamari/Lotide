16cdaf358bbca5c68d539f4516a48156b0e68a7f
Mariya <va.mariya@yahoo.com>
2021-11-03 23:32:28 +0000

    commiting assertArrayEqualTest

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,19 @@
+const assertEqual = require('../assertEqual');
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+assertArraysEqual([1,2,3], [1,2,3])
+assertArraysEqual([1,2,3], [1,3,4,5])
