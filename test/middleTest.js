162689f62f0e439ad065765ccafbb50713e0cffe
Mariya <va.mariya@yahoo.com>
2021-11-04 20:20:51 +0000

    commiting tests with Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,16 +1,16 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
 
-
-// const testArray = ["a","b","c","d"]
+const testArray = ["a","b","c","d"]
 // console.log(middle(testArray))
 
 
 
 describe("#middle", () => {
-  it("returns index:2, ['b', 'c'] for ['a', 'b', 'c', 'd']", () => {
-    assert.strictEqual(middle(testArray));
+  it("returns  ['b', 'c'] for ['a', 'b', 'c', 'd']", () => {
+    assert.deepEqual(middle(testArray),['b', 'c']);
   });
 
 });
